10ae3a230ac4c10f0a67b14cbf755569f86805c2
Ben Neuman <ben.neuman@gmail.com>
2014-02-03 18:44:12 -0600

    basic functionality

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -2,3 +2,39 @@ get '/' do
   # Look in app/views/index.erb
   erb :index
 end
+
+post '/create_players' do
+  p1 = Player.create(name: params[:player1])
+  p2 = Player.create(name: params[:player2])
+
+
+  redirect to("/game?p1=#{p1.id}&p2=#{p2.id}")
+end
+
+get '/game' do
+  @p1_name = Player.return_name(params[:p1])
+  @p2_name = Player.return_name(params[:p2])
+
+  erb :game
+end
+
+post '/log_winner' do
+  p1_id = Player.find_by_name(params[:p1]).id
+  p2_id = Player.find_by_name(params[:p2]).id
+  winner_id = Player.find_by_name(params[:winner]).id
+
+
+  game = Game.create(player1_id: p1_id,
+                     player2_id: p2_id,
+                     winner_id: winner_id)
+
+  game.id.to_s
+
+end
+
+get '/results' do
+  @p1name = Player.return_name(Game.find(params[:game_id]).player1_id)
+  @p2name = Player.return_name(Game.find(params[:game_id]).player2_id)
+  @winner_name = Player.return_name(Game.find(params[:game_id]).winner_id)
+  erb :results
+end
--- public/js/application.js
+++ public/js/application.js
@@ -1,7 +1,63 @@
 $(document).ready(function() {
-  // This is called after the document has loaded in its entirety
-  // This guarantees that any elements we bind to will exist on the page
-  // when we try to bind to them
 
-  // See: http://docs.jquery.com/Tutorials:Introducing_$(document).ready()
-});
+  var p1Name = $('#player1_strip').attr('data-player-name');
+  var p2Name = $('#player2_strip').attr('data-second-player-name');
+
+  var items = $('.spot');
+  var items2 = $('.spot2');
+
+  var restart = function() {
+
+    items.first().addClass('active');
+    items2.first().addClass('active');
+  }; // end function
+
+
+
+  restart();
+
+  // console.log(items);
+  // var play = function() {
+  $(window).keyup(function(e) {
+      var old = items.filter('.active');
+      var current;
+
+      var old2 = items2.filter('.active');
+      var current2;
+
+      if (e.keyCode === 81) {
+          current = old.next();
+          old.removeClass('active');
+          current.addClass('active');
+
+        if ( items.last().hasClass( "active" ) ) {
+          window.alert(p1Name + " is triumphant");
+          $.post('/log_winner', {p1: p1Name, p2: p2Name, winner: p1Name})
+            .done(function( gameId ) {
+              console.log(gameId)
+              window.location.href="/results?game_id=" + gameId;
+          });
+        }; // end inner if
+      };// end outer if
+
+
+
+
+      if (e.keyCode === 80) {
+          current2 = old2.next();
+          old2.removeClass('active');
+          current2.addClass('active');
+
+        if ( items2.last().hasClass( "active" ) ) {
+          window.alert(p2Name + " is triumphant");
+          $.post('/log_winner', {p1: p1Name, p2: p2Name, winner: p2Name}).done(function(){
+            window.location.href="/results";
+          });
+        }; // end inner
+      }; // end outer
+  }); //end listener
+
+  // }; //end function
+// restart();
+// play();
+}); //end document
